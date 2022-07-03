12717ce928e2264a00bd2bc6f1011d73e8f4804c
minseb96 <minseb96@naver.com>
2022-07-04 02:11:44 +0900

    스프링이 제공하는 ExceptionResolver1 완료.

---
+++ Spring/exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,28 @@
+package hello.exception.resolver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        try {
+            if (ex instanceof IllegalArgumentException) {
+                log.info("IllegalArgumentException resolver to 400");
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                return new ModelAndView();
+            }
+        } catch (IOException e) {
+            log.error("resolver ex", e);
+        }
+
+        //null return이면, 또 다른 실행할 ExceptionResolver가 있는지 확인해보고 없으면 원래 기존에 발생한 exception을 그대로 WAS에 전파시킴
+        return null;
+    }
+}
